60922d78472192c9e7dc21ce3fe746b22410bfe9
Abraham <athioii6@gmail.com>
2022-10-25 15:43:43 +0700

    Developing sleepless function

--- registerwa.py
+++ registerwa.py
@@ -1,9 +1,13 @@
 import os
 import helper
 from time import sleep
+import subprocess
+import uiautomator2 as u2
 
+d = u2.connect("R9CT4007GBM")
 device_id = "R9CT4007GBM"
 phone_num = "87815494888"
+packagename = "com.whatsapp"
 
 def chooseLanguage():
     os.system(f'adb -s ' + device_id + ' shell input tap 525 1023')
@@ -11,12 +15,15 @@ def chooseLanguage():
 
 def frontPageLanguage():
     chooseLanguage()
-    os.system(f'adb -s ' + device_id + ' shell input tap 650 1660')
-    os.system(f'adb -s ' + device_id + ' shell input tap 515 600')
-    for i in phone_num:
-        helper.pressKey(i)
-    os.system(f'adb -s ' + device_id + ' shell input tap 530 1250')
-
+    status = os.system(f'adb -s R9CT4007GBM shell dumpsys window | findstr "mCurrentFocus"')
+    if status == " mCurrentFocus=Window{23d25ab u0 com.whatsapp/com.whatsapp.registration.EULA}":
+        os.system(f'adb -s ' + device_id + ' shell input tap 650 1660') 
+        os.system(f'adb -s ' + device_id + ' shell input tap 515 600')
+        for i in phone_num:
+            helper.pressKey(i)
+        os.system(f'adb -s ' + device_id + ' shell input tap 530 1250')
+    else:
+        return
 def frontPage():
     # Selamat datang di whatsapp
     os.system(f'adb -s ' + device_id + ' shell input tap 515 2060')
@@ -25,16 +32,39 @@ def frontPage():
     # Masukkan nomor telepon
     for i in phone_num:
         helper.pressKey(i)
+    # Check ada perlu pencet oke atau tidak
+        # Pengecekan
+    proc = subprocess.Popen('adb -s R9CT4007GBM shell dumpsys window | findstr "mFocusedApp"', stdout=subprocess.PIPE, shell=True)
+    (out, _) = proc.communicate()
+    data = str(out.decode("utf-8"))
+    print("proc = " + data)
+    while "com.whatsapp/.registration.VerifyPhoneNumber" in data:
+        print("Masuk bro")
+        break
+        # Kondisi
+    if proc == "mFocusedApp=ActivityRecord{4f59308 u0 com.whatsapp/.registration.RegisterPhone t361}":
+        # Klik oke
+        os.system(f'adb -s ' + device_id + ' shell input tap 889 1370')
+    else:
     # Klik lanjutkan    
-    os.system(f'adb -s ' + device_id + ' shell input tap 530 1250')
+        os.system(f'adb -s ' + device_id + ' shell input tap 530 1250')
 
+        
 def contactMediaPerm():
-    # Klik lanjut
-    os.system(f'adb -s ' + device_id + ' shell input tap 755 1585')
-    # Klik izinkan kontak
-    os.system(f'adb -s ' + device_id + ' shell input tap 535 1980')
-    # Klik izinkkan media
-    os.system(f'adb -s ' + device_id + ' shell input tap 535 1980')
+    status = os.system(f'adb -s R9CT4007GBM shell dumpsys window | findstr "mCurrentFocus"')
+    if status == "mCurrentFocus=Window{b356581 u0 com.whatsapp/com.whatsapp.RequestPermissionActivity}":
+        # Klik lanjut
+        os.system(f'adb -s ' + device_id + ' shell input tap 755 1585')
+        # Pengecekan
+        status = os.system(f'adb -s R9CT4007GBM shell dumpsys window | findstr "mCurrentFocus"')
+        if status == "mCurrentFocus=Window{6734374 u0 com.google.android.permissioncontroller/com.android.permissioncontroller.permission.ui.GrantPermissionsActivity}":
+            # Klik izinkan kontak
+            os.system(f'adb -s ' + device_id + ' shell input tap 535 1980')
+            # Pengecekan 
+            status = os.system(f'adb -s R9CT4007GBM shell dumpsys window | findstr "mCurrentFocus"')
+            if status == "mCurrentFocus=Window{6734374 u0 com.google.android.permissioncontroller/com.android.permissioncontroller.permission.ui.GrantPermissionsActivity}":
+                # Klik izinkkan media
+                os.system(f'adb -s ' + device_id + ' shell input tap 535 1980')
 
 def drivePerm():
     # Klik lewati
@@ -52,11 +82,10 @@ def profileSetup(name):
     # Klik lanjut
     os.system(f'adb -s ' + device_id + ' shell input tap 525 1300')
 
+def mainFunction():
+    frontPage()
+    #contactMediaPerm()
+    #drivePerm()
+    #profileSetup('Justin Timberlake')
 
-frontPage()
-sleep(5)
-contactMediaPerm()
-sleep(5)
-drivePerm()
-sleep(5)
-profileSetup('Justin Timberlake')
+mainFunction()
